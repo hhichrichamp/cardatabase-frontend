bd22360b53941428e15f0361d6e240e8ada29bb3
hhichriChamp <haikel.amazon@gmail.com>
2025-11-24 18:49:35 -0500

    Remove unused React import to fix TS6133

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+
+import  { useState } from "react";
 import {Header} from "./pages/Header";
 import { Menu } from "./pages/Menu";
 import  {Footer } from "./pages/Footer";
